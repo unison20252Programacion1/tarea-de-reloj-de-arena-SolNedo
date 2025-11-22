c32f48536e604da618df822b902f85aebb1838ce
SolNedo <InfernosDantesLCC@gmail.com>
2025-11-22 12:13:31 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -6,15 +6,15 @@ def reloj_arena(m: int, s: str) -> str:
         print("Error: La altura debe ser un entero positivo")
         return
     # imprimir reloj de arena
-    for i in range(row, 0, -1):
-        for j in range(row - i):
+    for i in range(m, 0, -1):
+        for j in range(m - i):
             print(" ", end="")
         for k in range(1, i * 2):
             print(s, end="")
         print()
     # parte inferior
-    for i in range(2, row + 1):
-        for j in range(row - i):
+    for i in range(2, m + 1):
+        for j in range(m - i):
             print(" ", end="")
         for k in range(1, i * 2):
             print(s, end="")
